1846ea23f10ffd71af9264a011268e482a60e4f8
Pavel Ivanov <pasha850@yandex.ru>
2019-07-27 20:06:41 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,2 @@
+PROXY = {"proxy_url": "socks5://t1.learn.python.ru:1080", "urllib3_proxy_kwargs": {"username": "learn", "password": "python"}}
+API_KEY = "930909436:AAH6aLutWlstMhauF_CvA1Hk42WQQdA0AVg"
